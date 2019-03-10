8d27d41da7a6c8756d4695633083f37d392412b6
João Paulo <joao.schmidt@viaflow.com.br>
2019-03-10 12:57:57 -0300

    adição do metodo para novo login

--- src/app/components/security/auth.interceptor.ts
+++ src/app/components/security/auth.interceptor.ts
@@ -1,6 +1,7 @@
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpResponse } from '@angular/common/http';
 import { SharedService } from 'src/app/services/shared.service';
 import { Observable } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 
 @Injectable()
@@ -20,7 +21,18 @@ export class AuthInterceptor implements HttpInterceptor {
                     Authorization: this.shared.token
                 }
             });
-            return next.handle(authRequest);
+            return next.handle(authRequest).pipe(
+                map((event: HttpEvent<any>) => {
+                    if (event instanceof HttpResponse) {
+                        if (event.status === 401) {
+                            this.shared.token = null;
+                            this.shared.user = null;
+                            window.location.href = '/login';
+                            window.location.reload();
+                        }
+                    }
+                    return event;
+                }));
         } else {
             return next.handle(req);
         }
